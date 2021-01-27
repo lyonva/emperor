bb56815b7ac3cff41337551b4acd2b123ccfa336
Leonardo Villalobos <leonardo.villalobosarias@ucr.ac.cr>
2021-01-26 18:36:51 -0600

    Data select now moves already-used datasets into an archive. New selected data does not repeat data in archive.

--- data/data_select.py
+++ data/data_select.py
@@ -1,13 +1,23 @@
 import pandas as pd
 import os
 import numpy as np
-from shutil import copyfile
+from shutil import copyfile, move
 
-def data_select(source, destination, size):
+def data_move(source, destination):
+    # Determine files
+    source_files = [f for f in os.listdir(source) if f.endswith('.csv')]
+    for file in source_files:
+        move( os.path.join(source, file), os.path.join(destination, file) )
+
+def data_select(source, destination, size, other=None):
     # Determine files that haven't been transfered
     source_files = [f for f in os.listdir(source) if f.endswith('.csv')]
     dest_files = [f for f in os.listdir(destination) if f.endswith('.csv')]
     diff_files = [ f for f in source_files if f not in dest_files ]
+    # If historic directory is specified, remove from pool
+    if other is not None:
+        other_files = [f for f in os.listdir(other) if f.endswith('.csv')]
+        diff_files = [ f for f in diff_files if f not in other_files ]
     
     # Select size amount of files to transfer
     if len(diff_files) > size:
@@ -24,5 +34,7 @@ def data_select(source, destination, size):
 if __name__ == "__main__":
     source = "./data_use"
     destination = "./data_selected"
+    historic = "./data_archive"
     
-    data_select(source, destination, 32)
+    data_move(destination, historic) # Move already used data
+    data_select(source, destination, 32, other=historic)
